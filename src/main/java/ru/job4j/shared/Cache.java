fd4d3ac9bca0d5c76f3ef4296888da00520c860f
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-06-23 21:11:58 +0300

    1. Синхронизация общих ресурсов.[#309561]

---
+++ src/main/java/ru/job4j/shared/Cache.java
@@ -0,0 +1,13 @@
+package ru.job4j.shared;
+
+public class Cache {
+
+    private static Cache cache;
+
+    public static synchronized Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}

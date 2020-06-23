8d59f11520271615eb06b5ed377980abfe92dacf
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-06-23 21:56:26 +0300

    3. Immutable объекты[#309559]

---
+++ src/main/java/ru/job4j/linked/Node.java
@@ -0,0 +1,24 @@
+package ru.job4j.linked;
+
+import net.jcip.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public class Node<T> {
+
+    private final Node<T> next;
+
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}

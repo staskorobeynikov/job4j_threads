d0d1c185fe2f7a12880a0f9e903f3952633cccbb
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-06-26 12:54:56 +0300

    2. Обеспечить остановку потребителя.[#309545]

---
+++ src/main/java/ru/job4j/waitnotify/ParallelSearch.java
@@ -0,0 +1,34 @@
+package ru.job4j.waitnotify;
+
+public class ParallelSearch {
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        producer.join();
+        consumer.interrupt();
+    }
+}

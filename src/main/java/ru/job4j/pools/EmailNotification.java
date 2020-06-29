c56023cd57190b68e6773c64ede57d3098e07d6f
Stas Korobeynikov <stas.korobeinikov@mail.ru>
2020-06-29 19:55:09 +0300

    2. ExecutorService рассылка почты.[#309541]

---
+++ src/main/java/ru/job4j/pools/EmailNotification.java
@@ -0,0 +1,38 @@
+package ru.job4j.pools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public void emailTo(User user) {
+        String subject = String.format(
+                "Notification %s to email %s.",
+                user.getUsername(),
+                user.getEmail()
+        );
+        String body = String.format(
+                "Add a new event to %s.",
+                user.getUsername()
+        );
+        pool.submit(() -> send(subject, body, user.getEmail()));
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public void send(String subject, String body, String email) {
+    }
+}
